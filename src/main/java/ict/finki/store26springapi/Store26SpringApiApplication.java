6c4cb7028f3a08d1b19235ddd4b1c2b395b8b1e4
MartinAdzhiev <adzhievmartin@gmail.com>
2024-03-31 18:04:45 +0200

    Initial project

---
+++ src/main/java/ict/finki/store26springapi/Store26SpringApiApplication.java
@@ -0,0 +1,13 @@
+package ict.finki.store26springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Store26SpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Store26SpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ict/finki/store26springapi/Store26SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ict.finki.store26springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Store26SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
